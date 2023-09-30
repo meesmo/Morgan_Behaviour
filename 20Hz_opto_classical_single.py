ea251c36a62831adb9be2a39321c972f85ef336e
meesmo <142918054+meesmo@users.noreply.github.com>
2023-09-30 18:54:40 -0400

    Update 20Hz_opto_classical_single.py
    line 123 capital R on Rule

--- 20Hz_opto_classical_single.py
+++ 20Hz_opto_classical_single.py
@@ -120,7 +120,7 @@ for trial in trials:
     data.sample_tone[trial] = 'R'
     data.t_sample_tone[trial] = time.time() * 1000 - data._t_start_abs[trial]
 
-    tone = rule.R_tone
+    tone = Rule.R_tone
     if trial < 30:
         _opto_trial = False
     elif trial > 30:
